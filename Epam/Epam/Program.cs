de12a61c23f5eff38d1f4ce730453f908d4981b7
thecamdead <valshot73@gmail.com>
2020-11-12 14:50:37 +0200

    Epam

---
+++ Epam/Epam/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Epam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
